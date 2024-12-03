64ee84c3faa2ea0a2700e81b6b3b0ce272a63912
manohar.v@nyros.com <manohar.v@nyros.com>
2024-12-03 16:33:24 +0530

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  server:{
-    port: 3000
-  }
-})
+  server: {
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0",
+  },
+});
